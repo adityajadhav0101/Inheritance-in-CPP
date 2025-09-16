0647ab12a9a584818ea3f78c4ed370b087d852c7
Aditya Jadhav <adityabjadhav1252@gmail.com>
2025-09-16 20:15:39 +0530

    Add files via upload

---
+++ Single Inheritance.cpp
@@ -0,0 +1,32 @@
+// Aditya Jadhav
+// 24070123005
+
+#include<iostream>
+using namespace std;
+
+class Uni {
+public:
+    string uni = "Symbiosis";
+    void discipline() {
+        cout << "Engineering" << endl;
+    }
+};
+
+class Dep : public Uni {
+public:
+    string dept = "Electronics & Communication";
+};
+
+int main() {
+    Dep department;
+    cout << department.uni << endl;
+    department.discipline();
+    cout << department.dept << endl;
+
+    return 0;
+}
+
+// OUTPUT
+// Symbiosis
+// Engineering
+// Electronics & Communication
